1260aced2798d237d23a32183c21d453f9c7d2b4
syswcq-xhs <481124168@qq.com>
2025-10-23 22:23:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def isOdd(n):
-    # 先判断是否是整数（排除bool类型，因为bool是int的子类）
-    if isinstance(n, int) and not isinstance(n, bool):
-        # 是整数，再判断是否为奇数
-        return n % 2 != 0
-    # 非整数或其他情况，返回False
-    return False    
+def is_prime(num):
+    if num <= 1:
+        return False
+    if num == 2:
+        return True
+    if num % 2 == 0:
+        return False
+    for i in range(3, int(num**0.5) + 1, 2):
+        if num % i == 0:
+            return False
+    return True
